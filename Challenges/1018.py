e11ba1546c4872856dabd1d4176e53215db4a8e8
Matheus Hamada <matheushamada@live.com>
2021-07-28 18:33:39 -0300

    URI

--- Challenges/1018.py
+++ Challenges/1018.py
@@ -1,29 +1,40 @@
-n = int(input())
-print(n)
-n100 = n / 100
-n = n - n100*100
+value = int(input());
+val = value;
+cem = cinquenta = vinte = dez = cinco = dois = um = 0;
 
-n50 = n / 50
-n = n - n50*50
+if int(value/100) >= 1:
+	cem = int(value/100);
+	value -= cem*100;
 
-n20 = n / 20
-n = n - n20*20
+if int(value/50) >= 1:
+	cinquenta = int(value/50);
+	value -= cinquenta*50;
 
-n10 = n / 10
-n = n - n10*10
+if int(value/20) >= 1:
+	vinte = int(value/20);
+	value -= vinte*20;
 
-n5 = n / 5
-n = n - n5*5
+if int(value/10) >= 1:
+	dez = int(value/10);
+	value -= dez*10;
 
-n2 = n / 2
-n = n - n2*2
+if int(value/5) >= 1:
+	cinco = int(value/5);
+	value -= cinco*5;
 
-n1 = n / 1
-n = n - n1*1
-print('{} nota(s) de R$ 100,00'.format(n100))
-print('{} nota(s) de R$ 50,00'.format(n50))
-print('{} nota(s) de R$ 20,00'.format(n20))
-print('{} nota(s) de R$ 10,00'.format(n10))
-print('{} nota(s) de R$ 5,00'.format(n5))
-print('{} nota(s) de R$ 2,00'.format(n2))
-print('{} nota(s) de R$ 1,00'.format(n1))
+if int(value/2) >= 1:
+	dois = int(value/2);
+	value -= dois*2;
+
+if int(value/1) >= 1:
+	um = int(value/1);
+	value -= um*1;
+
+print(".format(){}" .format(val));
+print(".format(){} nota(s) de R$ 100,00" .format(cem));
+print(".format(){} nota(s) de R$ 50,00" .format(cinquenta));
+print(".format(){} nota(s) de R$ 20,00" .format(vinte));
+print(".format(){} nota(s) de R$ 10,00" .format(dez));
+print(".format(){} nota(s) de R$ 5,00" .format(cinco));
+print(".format(){} nota(s) de R$ 2,00" .format(dois));
+print(".format(){} nota(s) de R$ 1,00" .format(um));
