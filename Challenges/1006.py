c24b01eeb6335bb36abb1137d8034d3ce7fd980a
Matheus Hamada <matheushamada@live.com>
2021-07-28 15:19:26 -0300

    URI

---
+++ Challenges/1006.py
@@ -0,0 +1,10 @@
+a = float(input())
+b = float(input())
+c = float(input())
+
+a = a * 2
+b = b * 3
+c = c * 5
+av = (a + b + c)/10
+
+print('MEDIA = {}'.format('%.1f' % av))
---
+++ Challenges/1008.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = float(input())
+c = float(input())
+
+number = a
+salary = b * c
+
+
+print('NUMBER = {}'.format(number))
+print('SALARY = U$ {}'.format('%.2f' % salary))
